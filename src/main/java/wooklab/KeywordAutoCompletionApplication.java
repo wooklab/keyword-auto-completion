c7487cccb93b626f595e931f052995fc6baed7b0
In-wook Hwang <inwook9@gmail.com>
2022-05-12 01:00:41 +0900

    initial commit

---
+++ src/main/java/wooklab/KeywordAutoCompletionApplication.java
@@ -0,0 +1,13 @@
+package wooklab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeywordAutoCompletionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeywordAutoCompletionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/wooklab/KeywordAutoCompletionApplicationTests.java
@@ -0,0 +1,13 @@
+package wooklab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeywordAutoCompletionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
